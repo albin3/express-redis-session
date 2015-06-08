4fe3071a5fb042c33872f8cab5438f9fa16b9e93
albin3 <albin@louding.com>
2015-06-09 00:00:05 +0800

    add mocha test case

---
+++ test/miniExpress.js
@@ -0,0 +1,67 @@
+//模拟实现express，避免在测试代码中引入Express
+//Albin Zeng
+//2015-06-05
+var http = require('http');
+var server = http.createServer();
+
+function express() {
+
+  //express的中间件,第一个函数为CookieParser功能
+  server.middleware = [cookieParser];
+
+  //实现express的use功能
+  server.use = function(f) {
+    server.middleware.push(f);
+  };
+
+  //递归循环调用middleware
+  server.helper = function(req, res, idx) {
+    if (typeof server.middleware[idx] != 'undefined') {
+      server.middleware[idx](req, res, function(err) {
+        if (!err) {
+          return server.helper(req, res, idx+1);
+        } else {
+          return res.write('middle error');
+        }
+      });
+    }
+  }
+
+  server.on('request', function(req, res) {
+    server.helper(req, res, 0);
+  });
+  return server;
+};
+
+//cookieParser通过分析headers解析为cookie
+function cookieParser(req, res, next) {
+
+  var headers = req.headers || {};
+  var cookie = headers['cookie'] || '';
+  var listCookies = cookie.split(';');
+
+  var mapCookies = {};
+  for (var i=0; i<listCookies.length; i++) {
+    var idx = listCookies[i].indexOf('=');
+    var key;
+    var value;
+    if (idx != -1) {
+      key = listCookies[i].slice(0,idx).replace(/(^\s*)|(\s*$)/g,'');
+      value = listCookies[i].slice(idx+1).replace(/(^\s*)|(\s*$)/g,'');
+    } else {
+      key = 'key';
+      value = 'value';
+    }
+    mapCookies[key] = value;
+  }
+  req.cookies = mapCookies;
+
+  res.cookie = function(key, value) {
+    listCookies.push(key+'='+value);
+    res.setHeader("Set-Cookie", listCookies);
+  }
+
+  next();
+}
+
+module.exports = express;
---
+++ test/test-node-redis-session.js
@@ -0,0 +1,100 @@
+//test case
+var assert = require('assert');
+var nodeRedisSession = require('../');
+var express = require('./miniExpress');
+var http = require('http');
+
+var app = express();
+app.use(nodeRedisSession.get_redis_session("default"));
+app.use(function(req, res) {
+  if (req.session)
+    res.write(JSON.stringify(req.session));
+  res.write(';');
+  req.session = {randomSort: [1,2,3,4,5,6].sort(function() {if (Math.random()>0.5) return true})};
+  res.end(JSON.stringify(req.session));
+});
+app.listen(3009);
+
+describe('#nodeRedisSession', function(){
+
+  //用于比较两次返回的Session，prev为上次设置的Session，current为本次获得的Session
+  var prev = '';
+  var current = '';
+  var setCookie;
+
+  describe('#first request', function(){
+    http.request({
+      hostname: 'localhost',
+      port: 3009,
+      path: '/',
+      method: 'GET'
+    }, function(res) {
+      describe('#getFirstResponse', function() {
+        it('statusCode should be 200.', function() {
+          assert.equal(res.statusCode, 200);
+        })
+        it('set-cookie should be in headers', function() {
+          if (typeof res.headers['set-cookie'] == 'undefined') {
+            throw 'no set-cookie in res.headers.';
+          } else {
+            setCookie = res.headers['set-cookie'];
+          }
+        })
+      });
+      res.on('data', function(chunk) {
+        describe('#firstRequestGetData', function() {
+          it('data should not null', function() {
+            this.timeout(500);
+            var data = chunk.toString();
+            prev = data.split(';')[1];
+          })
+          it('check prev', function() {
+            assert(typeof prev, 'string');
+            describe('#secondRequest', function() {
+              var cookie = '';
+              for (var i=0; i<setCookie.length; i++) {
+                cookie += setCookie[i]+';';
+              }
+              cookie = cookie.slice(0,-1);
+              http.request({
+                hostname: 'localhost',
+                port: 3009,
+                path: '/',
+                method: 'GET',
+                headers: {
+                  cookie: cookie
+                }
+              }, function(res) {
+                describe('#secondRequestResponse', function() {
+                  it('statusCode should 200', function() {
+                    assert(res.statusCode, 200);
+                  })
+                  it('no set-cookie should appear', function() {
+                    if (res.headers['set-cookie']) {
+                      throw 'error';
+                    }
+                  })
+                })
+                res.on('data', function(chunk) {
+                  describe('#response data check', function() {
+                    var current = chunk.toString().split(';')[0];
+                    it('prev session SHOULD EQUAL current session', function() {
+                      assert(prev, current);
+                    });
+                  })
+                })
+              }).end();
+              it('second request sent.', function() {
+                this.timeout(500);
+              })
+            })
+          })
+        })
+      });
+    }).end();
+    it('500ms timeout', function(done) {
+      this.timeout(500);
+      setTimeout(done, 200);
+    })
+  })
+})
