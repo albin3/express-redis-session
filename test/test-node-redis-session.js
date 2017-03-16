36ea02c791385e54928577544af1cdffad98b638
马肱 <binwei.zbw@alibaba-inc.com>
2017-03-16 14:47:52 +0800

    feature: support getRedisClient.

--- test/test-node-redis-session.js
+++ test/test-node-redis-session.js
@@ -289,3 +289,100 @@ describe('#sso && app cache support', function() {
   });
 });
 
+describe('#getRedisClient support', function() {
+  before(function(done) {
+    var app = express();
+    var redisClient = new Redis();
+    function getRedisClient() {
+      return redisClient;
+    }
+    app.use(redisSession({ 
+      getRedisClient,
+      cookieName: 'sid#test',
+      cacheCookieName: 'sid#cache',
+      expireTime: 24*3600*1000 
+    }));
+
+    app.use(function(req, res) {
+      if (req.cache) {
+        res.write(JSON.stringify(req.cache));
+      }
+      res.write(';');
+      req.cache = {
+        randomSort: [1,2,3,4,5,6]
+                    .sort(function() {
+                            if (Math.random()>0.5) return true
+                          })
+      };
+      res.end(JSON.stringify(req.cache));
+    });
+
+    app.listen(3018);
+
+    setTimeout(function() {
+      done();
+    }, 300);
+  });
+
+  it('first request', function(done) {
+    http.request({
+      hostname: '127.0.0.1',
+      port: 3018,
+      path: '/',
+      method: 'GET'
+    }, function(res) {
+      assert.equal(res.statusCode, 200);
+      assert.equal(typeof res.headers['set-cookie'], 'object');
+      var setCookie = res.headers['set-cookie'];
+      cookie = '';
+      for (var i=0; i<setCookie.length; i++) {
+        cookie += setCookie[i]+';';
+      }
+      cookie = cookie.slice(0,-1);
+
+      res.on('err', function(err) {
+        done(err);
+      });
+
+      var data = '';
+      res.on('data', function(chunk) {
+        data += chunk.toString();
+      });
+      res.on('end', function() {
+        prev = data.split(';')[1];
+        assert(typeof prev, 'string');
+        done();
+      });
+    }).end();
+  });
+
+  it('second request', function(done) {
+    http.request({
+      hostname: '127.0.0.1',
+      port: 3018,
+      path: '/',
+      method: 'GET',
+      headers: {
+        cookie: cookie
+      }
+    }, function(res) {
+      assert.equal(res.statusCode, 200);
+      assert.equal(typeof res.headers['set-cookie'], 'undefined');
+
+      res.on('err', function(err) {
+        done(err);
+      });
+
+      var data = '';
+      res.on('data', function(chunk) {
+        data += chunk.toString();
+      });
+      res.on('end', function() {
+        current = data.split(';')[0];
+        assert.equal(prev, current);
+        done();
+      });
+    }).end();
+  });
+});
+
